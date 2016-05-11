5af281c32b0847a2bd40b8ab833083a9c88ddf00
Marcus Brohede <a97marbr@gmail.com>
2016-05-11 15:08:39 +0200

    Missed one

--- DuggaSys/templates/shapes-dugga.js
+++ DuggaSys/templates/shapes-dugga.js
@@ -112,14 +112,15 @@ function reset()
 
 }
 
-function showFacit(param, uanswer, danswer, userStats)
+function showFacit(param, uanswer, danswer, userStats, files, moment)
 {
-	document.getElementById('duggaTime').innerHTML=userStats[0];
-	document.getElementById('duggaTotalTime').innerHTML=userStats[1];
-	document.getElementById('duggaClicks').innerHTML=userStats[2];
-	document.getElementById('duggaTotalClicks').innerHTML=userStats[3];
-	$("#duggaStats").css("display","block");
-
+	if (userStats != null){
+		document.getElementById('duggaTime').innerHTML=userStats[0];
+		document.getElementById('duggaTotalTime').innerHTML=userStats[1];
+		document.getElementById('duggaClicks').innerHTML=userStats[2];
+		document.getElementById('duggaTotalClicks').innerHTML=userStats[3];
+		$("#duggaStats").css("display","block");
+	}
 	var canvas = document.getElementById("myCanvas");
 	ctx = canvas.getContext("2d");
 
