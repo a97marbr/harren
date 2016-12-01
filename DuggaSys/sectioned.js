d792e9f9cb1c6589bb55e938c20045fd7ae077d5
Marcus Brohede <a97marbr@gmail.com>
2016-12-01 14:11:55 +0100

    completed the move back to resulted

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -459,7 +459,6 @@ function returnedSection(data)
 			str+="<td style='width:112px;'><input type='button' value='New version' class='submit-button' title='Create a new version of this course' onclick='showCreateVersion();'></td>";
 			str+="<td style='width:112px;'><input type='button' value='Access' class='submit-button' title='Give students access to the selected version' onclick='accessCourse();'/></td>";
 			str+="<td style='width:112px;'><input type='button' value='Results' class='submit-button' title='Edit student results' onclick='changeURL(\"resulted.php?cid="+querystring['courseid']+"&coursevers="+querystring['coursevers']+"\")' /></td>";
-			str+="<td style='width:112px;'><input type='button' value='Mark' class='submit-button' title='Edit student results' onclick='changeURL(\"markDugga.php?cid="+querystring['courseid']+"&coursevers="+querystring['coursevers']+"\")' /></td>";
       			
 			str+="<td style='width:112px;'><input type='button' value='Tests' class='submit-button' id='testbutton' onclick='changeURL(\"duggaed.php?cid="+querystring['courseid']+"&coursevers="+querystring['coursevers']+"\")'/></td>";
 			str+="<td style='width:112px;'><input type='button' value='Files' class='submit-button' onclick='changeURL(\"fileed.php?cid="+querystring['courseid']+"&coursevers="+querystring['coursevers']+"\")'/></td>";
